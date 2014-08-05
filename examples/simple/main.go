cc77ea3aef41c196ce2ea462214e3bb4d1e804f3
YU HengChun <achun.shx@qq.com>
2014-08-06 03:21:32 +0800

    simple example

---
+++ examples/simple/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/typepress/rivet"
+)
+
+func empty() { println("empty") }
+
+func HelloWorld(w http.ResponseWriter) {
+	w.Write([]byte("Hello World"))
+}
+
+func Hello(params rivet.Params, w http.ResponseWriter) {
+	w.Write([]byte("Hello " + params["name"].(string)))
+}
+
+func CatchAll(c rivet.Context) {
+	c.WriteString(c.PathParams().Get("*"))
+}
+
+func Go(c rivet.Injector) {
+	c.Map("Death is coming. Let's Go!")
+}
+
+func GoGo(c rivet.Injector) {
+	// 获取 string 类型标识
+	id := rivet.TypeIdOf("string")
+
+	c.WriteString(c.PathParams().Get("name") + "! " +
+		c.Get(id).(string)) // 类型转换
+}
+
+func main() {
+
+	// 传递 nil, NewRouter 使用内部的 Rivet 实现
+	mux := rivet.NewRouter(nil)
+
+	mux.Get("/", HelloWorld)
+	mux.Get("/empty", empty)
+	mux.Get("/hi/:name string 5", Hello)
+	mux.Get("/hi/**", CatchAll)
+	mux.Get("/:name", Go, GoGo)
+	_, route := mux.Match("GET", "/")
+	route.(*rivet.Trie).Print("")
+
+	// rivet.Router 符合 http.Handler 接口
+	http.ListenAndServe(":3000", mux)
+}
